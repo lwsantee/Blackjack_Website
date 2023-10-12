73e50cbf01c79857fdd8a35b4a891e21b8657ffa
Logan <lwsantee@protonmail.com>
2023-10-12 00:39:03 -0400

    format test code files

--- blackjack/__main__.py
+++ blackjack/__main__.py
@@ -1,17 +1,5 @@
-from .models.Card import Card
-from .models.Player import Player
+# run.py
+from blackjack import app
 
-# Create a sample player
-player1 = Player(name="Alice")
-
-# Add cards to the player's hand
-card1 = Card(suit="Hearts", rank="8")
-card2 = Card(suit="Diamonds", rank="K")
-
-player1.add_card_to_hand(card1)
-player1.add_card_to_hand(card2)
-
-# Print player details and hand value
-print(player1)
-print("Hand:", [str(card) for card in player1.hand])
-print("Total Hand Value:", player1.calculate_hand_value())
+if __name__ == '__main__':
+    app.run(debug=True)
