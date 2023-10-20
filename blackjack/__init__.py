fa1c8ddd285b86aad7cbfff2d398a7ace3b5f468
areif-dev <ajreifsnyder@protonmail.com>
2023-10-20 00:50:11 -0400

    Move views import to __main__ to avoid circular imports

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -1,4 +1,3 @@
-from .views import routes
 from flask import Flask
 
 app = Flask(__name__)
--- blackjack/__main__.py
+++ blackjack/__main__.py
@@ -1,4 +1,5 @@
 from blackjack import app
+from .views import routes
 
 if __name__ == '__main__':
     app.run(debug=True)
