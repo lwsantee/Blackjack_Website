995696af37c0bdbd9273fbc840d82f6f2d57f9b0
areif-dev <ajreifsnyder@protonmail.com>
2023-10-24 11:58:27 -0400

    Create test handlers for socket.io messages

--- blackjack/views/routes.py
+++ blackjack/views/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from blackjack import app
+from blackjack import socketio
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@socketio.on("my event")
+def handle_custom_event(json):
+    print("Message received: " + str(json))
