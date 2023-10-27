9891c5133b655d94fcbe2d3f6898df29f32fb703
areif-dev <ajreifsnyder@protonmail.com>
2023-10-27 12:27:20 -0400

    Move some REST endpoints from ws_handlers to rest_api.py

--- blackjack/__main__.py
+++ blackjack/__main__.py
@@ -2,6 +2,7 @@ from blackjack import app
 from blackjack import socketio
 from .views import routes
 from .controllers import ws_handlers
+from .controllers import rest_api
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", use_reloader=True, log_output=True)
---
+++ blackjack/controllers/rest_api.py
@@ -0,0 +1,35 @@
+import http
+from flask import Response, request
+from flask_socketio import emit, send
+from blackjack import socketio
+from blackjack.models.Player import Player
+from blackjack import app
+
+
+@app.route("/api/active-players", methods=["GET"])
+def handle_get_active_players():
+    return Player.get_active_players_json()
+
+
+@app.route("/api/player-join", methods=["POST"])
+def handle_player_join():
+
+    if request.get_json() is None:
+        return Response("Expected json payload", http.HTTPStatus.BAD_REQUEST)
+
+    required_fields = ["playerName", "playerBalance", "playerSeat"]
+    for field in required_fields:
+        if field not in request.get_json():
+            return Response(f"Missing field '{field}'", http.HTTPStatus.BAD_REQUEST)
+
+    new_player = Player.add_player(
+        request.get_json()["playerName"],
+        request.get_json()["playerBalance"],
+        request.get_json()["playerSeat"],
+    )
+    if new_player is None:
+        return Response("Player name is already in use", http.HTTPStatus.CONFLICT)
+
+    return {
+        "active_players": Player.get_active_players_json(),
+    }
