e0dca96c688072769b33d07e0210f71acfe5c928
Logan <lwsantee@protonmail.com>
2023-10-28 00:01:30 -0400

    change config stuff

--- blackjack/__main__.py
+++ blackjack/__main__.py
@@ -5,5 +5,5 @@ from .controllers import ws_handlers
 from .controllers import rest_api
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True,
+    socketio.run(app, host="192.168.0.24", port="5000", debug=True,
                  use_reloader=True, log_output=True)
