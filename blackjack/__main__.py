2c6be2857aa3704f842590a61559c4258ee9640c
Logan <lwsantee@protonmail.com>
2023-10-28 00:58:25 -0400

    change port

--- blackjack/__main__.py
+++ blackjack/__main__.py
@@ -5,5 +5,5 @@ from .controllers import ws_handlers
 from .controllers import rest_api
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host='0.0.0.0',
+    socketio.run(app, debug=True, host='0.0.0.0', port="6969",
                  use_reloader=True, log_output=True)
