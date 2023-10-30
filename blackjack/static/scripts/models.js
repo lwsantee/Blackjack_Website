65f335213acf39fce7f61cac8bcdc7aeb7b7d3d7
areif-dev <ajreifsnyder@protonmail.com>
2023-10-29 21:31:50 -0400

    Create models.js for storing typedefs

---
+++ blackjack/static/scripts/models.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {Object} Player
+ * @property {Card[]} hand
+ * @property {string} name
+ * @property {number} bet
+ * @property {number} seat
+ */
+
+/**
+ * @typedef {Object} Card
+ * @property {string} suit
+ * @property {string} rank
+ */
